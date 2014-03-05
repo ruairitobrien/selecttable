a2af0bb36197c10420f69c3b7707d42b12bbaa54
Ruairi O Brien <ruairitobrien@gmail.com>
2014-03-05 15:11:08 +0000

    Added main selection capabilities
    Also added a placeholder for more funcitonality.
    
    Also added some demo stuff.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-var aTest = $('#sample1').tableSelect();
-$('#sample1-output').text(aTest.test('Test 1'));
-
-var aTest = $('#sample2').tableSelect({
-	backgroundColor: "YELLOW"
+var sampleTable1 = $('#sample1');
+var sampleTable2 = $('#sample2');
+var test1 = sampleTable1.tableSelect();
+var test2 = sampleTable2.tableSelect({
+	selectClass: 'select'
 });
 
-$('#sample2-output').text(aTest.test('Test 2'));
+
+$('.datagrid td').attr('unselectable', 'on'); // Stop table cell highlighting on shift click in IE. See main.css for method in other browsers.
+
+var buildTestOutput = function(outputElementId, columnCount, rowCount, selectedRows){
+	$(outputElementId).html(
+		'<p>'+
+		'Columns: '+ columnCount +'</br>'+
+		'Total Rows: '+ rowCount +'</br>'+
+		'Selected Rows: '+ selectedRows +'</br>'+
+		'</p>');
+};
+
+var displayCurrentTestOutput = function() {
+	buildTestOutput('#sample1-output', test1.getColumnCount(), test1.getRowCount(), test1.getSelectedRows());
+	buildTestOutput('#sample2-output', test2.getColumnCount(), test2.getRowCount(), test2.getSelectedRows());
+} 
+
+// Initialize
+displayCurrentTestOutput();
+
+sampleTable1.click(function () {
+	buildTestOutput('#sample1-output', test1.getColumnCount(), test1.getRowCount(), test1.getSelectedRows());
+});
+
+sampleTable2.click(function() {
+	buildTestOutput('#sample2-output', test2.getColumnCount(), test2.getRowCount(), test2.getSelectedRows());
+});
 
 
--- js/selecttable.js
+++ js/selecttable.js
@@ -1,18 +1,130 @@
 ;(function ($) {
     $.fn.tableSelect = function (options) {
+        
+        //=======================================
+        // Properties
+        //=======================================
+        var $scope = this;
+
+        var tableRows;
+        var lastSelectedRow; 
+        var selectedRows; 
+
         var settings = $.extend({
             // Default options.
-            backgroundColor: "RED"
-            
+            //=================
+            // The class to be appliead to selected rows
+            selectedClass: 'selected',
+            // Insert a selection menu in to the table. This is just a convenience method.
+            // Any functionality used in this menu can be called externaly in a custom menu.
+            addMenu: false
         }, options);
-        //this.find( "td" ).css( "background-color", settings.backgroundColor );
-        
-        var test = function(text) {
-            return text;
+
+        // Initialize
+        (function() {
+            tableRows = $scope.find( "tbody tr" ); // Cache the last selected row. Primarily for shift key selection.
+            selectedRows = 0;
+            // If the addMenu flag is true, insert the menu to the top of the table.
+            if (settings.addMenu) {
+                var insertMenu = function () {
+
+                };
+            }
+        })();
+
+        //=======================================
+        // Initialization
+        //=======================================
+
+        // Affect only rows within the table body.
+        tableRows.click( function(e){
+            if (e.metaKey || e.ctrlKey) {
+                //Cmd+Click or Ctrl+Click
+                $(this).toggleClass(settings.selectedClass);
+            } else {
+                if (e.shiftKey) {
+                    //Shift-Click
+                    if(lastSelectedRow) {
+                        var currentRowIndex = tableRows.index($(this));
+                        var lastRowIndex = tableRows.index(lastSelectedRow);
+
+                        var start = lastRowIndex;
+                        var end = currentRowIndex;                         
+                        if (currentRowIndex < lastRowIndex) {
+                            start = currentRowIndex;
+                            end = lastRowIndex;
+                        } 
+                        var i;
+                        var count = tableRows.length; 
+                        for (i = start; i <= end; i++) {
+                            if(i >= 0 && i < count) {
+                                if(!(i === lastRowIndex)) {
+                                    $(tableRows[i]).addClass(settings.selectedClass);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    clearTableSelection();
+                }
+                $(this).addClass(settings.selectedClass);
+            }
+            lastSelectedRow = $(this);
+            updateSelectedRows();
+        });
+
+        //=======================================
+        // Private functions
+        //=======================================
+        var updateSelectedRows = function () {
+            selectedRows = tableRows.filter('.selected').length;
         };
+
+        var insertMenu = function () {
+
+        };
+    
+        //=======================================
+        // Public functions
+        //=======================================
+        
+        var clearTableSelection = function () {
+            tableRows.removeClass(settings.selectedClass);
+        };
+
+        var selectAllRows = function () {
+            tableRows.addClass(settings.selectedClass);
+        };
+
+        var getSelectedRows = function () {
+          return selectedRows;  
+        };
+
+        var getRowCount = function () {
+            return tableRows.length;
+        }
+
+        var getColumnCount = function () {
+            var columnCount = 0;
+            if(tableRows && tableRows.length > 0) {
+                $(tableRows[0]).children('td').each(function () {
+                    if ($(this).attr('colspan')) {
+                        columnCount += +$(this).attr('colspan');
+                    } else {
+                        columnCount++;
+                    }
+                });
+            }
+            return columnCount;
+        }
+        
         
         return {
-            test: test
+            clearTableSelection: clearTableSelection,
+            selectAllRows: selectAllRows,
+            getSelectedRows: getSelectedRows,
+            getRowCount: getRowCount,
+            getColumnCount: getColumnCount            
         };
         
     };
